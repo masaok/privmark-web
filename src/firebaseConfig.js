9a620c3a988926198b97c8c36c6350136873690c
Masao Kitamura <masaok@gmail.com>
2023-02-17 00:56:36 -0800

    Ran Firebase init

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsURxQcN7SShHjqjy5BC2qd_9Zgto86y0",
+  authDomain: "private-market-data.firebaseapp.com",
+  projectId: "private-market-data",
+  storageBucket: "private-market-data.appspot.com",
+  messagingSenderId: "126114183301",
+  appId: "1:126114183301:web:f6d25ddcc512ef6f1091ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
